37d4164c06dfeeed8d91017e873363e33a84f46c
rcharmeyer <rcharmeyer@hotmail.com>
2023-03-18 11:21:38 -0700

    Create a simpler example called Tips

--- src/app-root.tsx
+++ src/app-root.tsx
@@ -1,12 +1,15 @@
 import { Suspense } from "react"
 import { ErrorBoundary } from "react-error-boundary"
 import { ProductPage } from "./product"
+import { Tip, TipRoot } from "./tips"
+
+const Page = () => <TipRoot id="1" /> // ProductPage
 
 export function App () {
   return (
     <ErrorBoundary fallback={null}>
       <Suspense>
-        <ProductPage />
+        <Page />
       </Suspense>
     </ErrorBoundary>
   )
